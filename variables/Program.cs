59c7953aae24c8616f850ca562e15123271f4448
Michael Argo <mike.argo@gmail.com>
2024-03-25 15:01:59 -0500

    Add project files.

---
+++ variables/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace variables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int x = 2;
+            double d = 1.0d;
+            float f = 4.0f;
+            decimal n = 3.0m;
+
+            string s = "";
+            s = "numbers are" + x + d + f + n;
+            Console.WriteLine(s);
+
+            Console.WriteLine("Please enter your name _");
+            string t = Console.ReadLine();
+            Console.WriteLine("Your name is " + t);
+
+            Console.WriteLine("numbers are {0} {1} {2} {3}", x, d, f, n );
+            Console.WriteLine("numbers are" + x + d + f + n);
+            Console.ReadLine();
+
+        }
+    }
+}
